8871840175ed28726026bc1b4d2ac3cb6fb6b1e0
WMs784 <64195816+WMs784@users.noreply.github.com>
2021-09-28 11:14:49 +0900

    スナップショット

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,19 @@ import 'judge.dart';
 
 void main() => runApp(MyApp());
 
+/*<color name="teal_200">#FF03DAC5</color>
+<color name="teal_700">#FF018786</color>*/
+//const int _kPurplePrimaryValue = ;
+
 class MyApp extends StatelessWidget {//MyWidgetの配置を決めるクラス
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.purple,
+        brightness: Brightness.dark,
+        primarySwatch: Colors.teal[700],
+        accentColor: Colors.teal[300],
       ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -37,7 +42,9 @@ class MyWidget extends StatefulWidget {
 class _MyWidgetState extends State<MyWidget> {
   // データを宣言
   int count = 0;
-  String s = '18桁以下の数を入力してください';//デフォルトメッセージ
+  bool b = true;
+  String s = '18桁以下の正整数を入力してください';//デフォルトメッセージ
+  String e = '18桁以下の正整数を入力してください';
   // データを元にWidgetを作る
   @override
   Widget build(BuildContext context) {
@@ -45,8 +52,14 @@ class _MyWidgetState extends State<MyWidget> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
         TextField(
+          keyboardType: TextInputType.number,
           onChanged: (text){
-            count = int.parse(text);//入力内容を文字列として取得
+            try{
+              count = int.parse(text);
+              b = true;
+            }catch(exception){
+              b = false;
+            }
           },
         ),
         TextButton(
@@ -54,7 +67,8 @@ class _MyWidgetState extends State<MyWidget> {
             // データを更新する時は setState を呼ぶ
             setState(() {
               // データを更新
-              s = pri_fact(count);//先ほど作った関数に入れて文字列を更新
+              if(b)s = pri_fact(count);//先ほど作った関数に入れて文字列を更新
+              else s = e;
             });
           },
           child: Text('素因数分解'),
